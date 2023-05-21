167f3f0a3e896a6815e5740598a85e6965fefde1
amd <amd@devin.com>
2023-05-21 13:51:22 +0900

    MySQL 과 MariaDB 도 연결 설정.
    DBConnectionUtilTest.connmection() 테스트 성공함.

--- src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -1,7 +1,15 @@
 package hello.jdbc.connection;
 
 public abstract class ConnectionConst {
-    public static final String URL = "jdbc:h2:tcp://localhost/~/test2";
-    public static final String USERNAME = "sa";
-    public static final String PASSWORD = "";
+//    public static final String URL = "jdbc:h2:tcp://localhost/~/test2";
+//    public static final String USERNAME = "sa";
+//    public static final String PASSWORD = "";
+
+//    public static final String URL = "jdbc:mysql://localhost:3306/test2";
+//    public static final String USERNAME = "root";
+//    public static final String PASSWORD = "apmsetup";
+
+    public static final String URL = "jdbc:mariadb://localhost:3307/test2";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "apmsetup";
 }
