f29c6f8c55787d7c38d3b569fe294b248d08a979
amd <amd@devin.com>
2023-05-23 12:54:54 +0900

    AWS RDS 추가.

--- src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -12,4 +12,8 @@ public abstract class ConnectionConst {
     public static final String URL = "jdbc:mariadb://localhost:3307/test2";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "apmsetup";
+
+//    public static final String URL = "jdbc:mysql://rds-mysql.cwphkdgwvj5q.ap-northeast-2.rds.amazonaws.com/test2";
+//    public static final String USERNAME = "admin";
+//    public static final String PASSWORD = "#aw98313";
 }
